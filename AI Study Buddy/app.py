6dae27965989cbde1488c68b83585031f6a02416
Abdhesh <abdheshshah111@gmail.com>
2026-01-30 23:40:20 +0545

    Day 2: Added HTML templates

--- AI Study Buddy/app.py
+++ AI Study Buddy/app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "Hello!"
+    return render_template('index.html')
 
-if __name__ =='__main__':
+
+if __name__ == '__main__':
     app.run(debug=True)
