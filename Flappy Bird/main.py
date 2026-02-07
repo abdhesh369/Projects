eb2fb1fc2e59ff38795093570f63c1f30581220d
Abdhesh <abdheshshah111@gmail.com>
2026-02-07 15:13:34 +0545

    Refactor code structure for improved readability and maintainability

--- Flappy Bird/main.py
+++ Flappy Bird/main.py
@@ -1,7 +1,7 @@
 import random
 import sys
 import pygame
-from pygame.local import *
+from pygame.locals import *
 FPS = 32
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 800
@@ -15,6 +15,27 @@ Game_Sounds = {}
 Player = 'Public/bird.png'
 Background = 'Public/Background.jpg'
 Pipe = 'Public/Pipe.png'
+
+def welcome_screen():
+    playerx = int(SCREEN_WIDTH/5)
+    playery = int((SCREEN_HEIGHT - Game_Sprites['player'].get_height())/2)
+    messagex = int((SCREEN_WIDTH - Game_Sprites['message'].get_width())/2)
+    messagey = int(SCREEN_HEIGHT*0.13)
+    basex = 0
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYDOWN and (event.key == K_SPACE or event.key == K_UP):
+                return
+            else:
+                display.blit(Game_Sprites['background'], (0, 0))
+                display.blit(Game_Sprites['player'], (playerx, playery))
+                display.blit(Game_Sprites['message'], (messagex, messagey))
+                display.blit(Game_Sprites['base'], (basex, Ground_y))
+                pygame.display.update()
+                FPSCLOCK.tick(FPS)
 
 
 game_exit = False
@@ -32,8 +53,11 @@ Game_Sprites['number'] = (
     pygame.image.load('Public/8.png').convert_alpha(),
     pygame.image.load('Public/9.png').convert_alpha(),
 )
-Game_Sprites['message'] = pygame.image.load(
-    'Public/message.png').convert_alpha()
+import os
+if os.path.exists("Public/message.png"):
+    Game_Sprites['message'] = pygame.image.load("Public/message.png").convert_alpha()
+else:
+    print("Warning: message.png not found!")
 Game_Sprites['base'] = pygame.image.load('Public/base.png').convert_alpha()
 Game_Sprites['pipe'] = (
     pygame.transform.rotate(pygame.image.load(
